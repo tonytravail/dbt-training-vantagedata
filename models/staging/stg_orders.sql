fee6f8a60e11de9091f708ca85243f17be642cbe
tony.travail@outlook.com <tony.travail@outlook.com>
2022-09-13 13:14:07 +0000

    edit delivery_team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
     -- from raw orders
+    {{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
     o.orderid,
     o.orderdate, 
     o.shipdate,
@@ -17,9 +18,10 @@ select
     p.category,
     p.productname,
     p.subcategory,
-    {{ markup('ordersellingprice', 'ordercostprice') }} as markup
+    {{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+    delivery_team
 from {{ ref('raw_order') }} as o
 left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p on o.productid = p.productid
-
+left join {{ ref('delivery_team') }} as d on o.shipmode = d.shipmode
 {{ limit_data_in_dev('orderdate') }}
